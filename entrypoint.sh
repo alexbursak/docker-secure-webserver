4e164a445c034cf4cbd505c37c5155eb1c4b9235
alexbursak <alexbursak3@gmail.com>
2021-09-13 15:30:02 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 #service apache2 restart && tail -f /dev/null
-service apache2 restart && /toriptables3.py -l && /bin/bash
+#service apache2 restart && /toriptables3.py -l && /bin/bash
+
+# PHP
+service php7.4-fpm start
+
+# Apache2
+service apache2 restart
+
+service php7.4-fpm status
+service apache2 status
+service cron status
+
+/toriptables3.py -l
+
+#exec "$@"
+/bin/bash
